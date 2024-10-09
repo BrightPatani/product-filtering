c0869bfc669c55eb2acea8058fe7b374ac6ca7f4
BrightPatani <brightpatani98@gmail.com>
2024-10-09 16:44:13 +0100

    product

---
+++ public/app.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const filters = document.querySelectorAll('.check');
+    const products = document.querySelectorAll('.item');
+    const searchInput = document.getElementById('search');
+
+    // Filter products
+    filters.forEach(filter => {
+      filter.addEventListener('change', filterProducts);
+    });
+
+    //search input
+    searchInput.addEventListener('input', filterProducts);
+
+    function filterProducts() {
+      const activeFilters = Array.from(filters).filter(filter => filter.checked).map(filter => filter.id);
+      const searchTerm = searchInput.value.toLowerCase();
+
+      products.forEach(product => {
+        const category = product.classList[1];
+        const name = product.querySelector('h3').textContent.toLowerCase();
+
+        const matchesCategory = activeFilters.includes('all') || activeFilters.includes(category);
+        const matchesSearch = name.includes(searchTerm);
+
+        if (matchesCategory && matchesSearch) {
+          product.classList.remove('hidden');
+        } else {
+          product.classList.add('hidden');
+        }
+      });
+    }
+
+    // Initialise filtering
+    filterProducts();
+  });
